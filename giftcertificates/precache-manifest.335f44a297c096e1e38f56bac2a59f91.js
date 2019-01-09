cf22b42f9d445a2abf3573b3829679fdc80f9712
Ammar <brohiammar@gmail.com>
2019-01-09 15:29:03 +0100

    added React app with rinkeby deployment

---
+++ giftcertificates/precache-manifest.335f44a297c096e1e38f56bac2a59f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "403d65af27ad9d3ce215",
+    "url": "./static/css/main.f4afdfb6.chunk.css"
+  },
+  {
+    "revision": "403d65af27ad9d3ce215",
+    "url": "./static/js/main.403d65af.chunk.js"
+  },
+  {
+    "revision": "982a65d45edc340f6b6c",
+    "url": "./static/js/1.982a65d4.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ce9ebdcc6e22225ed0e8d401721d7f9c",
+    "url": "./index.html"
+  }
+];
